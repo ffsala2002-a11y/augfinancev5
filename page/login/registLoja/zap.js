1066036f1f24f6396561b3a3667c831a3a50b0b8
Diego Silva <ffsala2002@gmail.com>
2026-07-22 12:27:05 -0300

    Validação nos campos do nome e mensagem

--- page/login/registLoja/zap.js
+++ page/login/registLoja/zap.js
@@ -1,9 +1,15 @@
 const form = document.getElementById("form");
+const erroInput = document.querySelector(".erro-input");
+const erroTextArea = document.querySelector(".erro-textArea");
 const tempo = 600;
+const tempoInput = 2000;
+const tempoTextArea = 2000;
 let timeId;
+let timeIdInput;
+let timeTextArea;
 
 
-
+/* REFATORAR ESSE CÓDICO */
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -15,7 +21,15 @@ form.addEventListener('submit', (e) => {
         const mensagem = document.getElementById("textArea").value;
         const tel = 5563999789035;
 
-        if (!nome || !mensagem) return alert("Campos vazio!");
+        if (!nome) {
+            mostrarAlerta("Campo obrigatório", "erroInput");
+        };
+
+        if (!mensagem) {
+            mostrarAlerta("Campo obrigatório", "erroTextArea");
+        }
+
+        if(!nome || !mensagem) return;
 
         const texto = `Olá, me chamo ${nome}, ${mensagem}`;
         const textoFormatado = encodeURIComponent(texto);
@@ -24,4 +38,32 @@ form.addEventListener('submit', (e) => {
 
         window.open(url, '_blank')
     }, tempo)
-})
+});
+
+
+function mostrarAlerta(mensagem, tipo) {
+
+    if (tipo === "erroInput") {
+        erroInput.textContent = mensagem;
+
+        erroInput.classList.add("active");
+
+        clearTimeout(timeIdInput);
+
+        timeIdInput = setTimeout(() => {
+            erroInput.classList.remove("active")
+        }, tempoInput)
+    };
+
+    if (tipo === "erroTextArea") {
+        erroTextArea.textContent = mensagem;
+
+        erroTextArea.classList.add("active");
+
+        clearTimeout(timeTextArea);
+
+        timeTextArea = setTimeout(() => {
+            erroTextArea.classList.remove("active")
+        }, tempoTextArea)
+    }
+}
